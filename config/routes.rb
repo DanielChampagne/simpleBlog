29276a8c9e4b671cb176e63ecf5179e293f22109
Daniel Champagne <danchampagne@hotmail.com>
2020-05-07 16:32:02 -0400

    Destroy for comments and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
  root 'posts#index', as: 'home'
  get 'rails/info/routes' => 'routes'
  get 'about' => 'pages#about', as: 'about'
- resources :posts
+ resources :posts do
+  resources :comments
+ end
+
+ 
  
 end
